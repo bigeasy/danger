aa92290203db39d83a0ac0ddef7e0e5399f3d1e7
Alan Gutierrez <alan@blogometer.com>
2010-05-02 15:40:49 -0500

    Update build for new Mix project language.

--- src/mix/java/com/goodworkalan/danger/mix/DangerProject.java
+++ src/mix/java/com/goodworkalan/danger/mix/DangerProject.java
@@ -1,6 +1,5 @@
 package com.goodworkalan.danger.mix;
 
-import com.goodworkalan.go.go.Artifact;
 import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
@@ -10,15 +9,15 @@ public class DangerProject extends ProjectModule {
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
-                .produces(new Artifact("com.github.bigeasy.danger/danger/0.1"))
+                .produces("com.github.bigeasy.danger/danger/0.1")
                 .main()
                     .depends()
-                        .artifact(new Artifact("com.github.bigeasy.verbiage/verbiage/0.1"))
+                        .include("com.github.bigeasy.verbiage/verbiage/0.1")
                         .end()
                     .end()
                 .test()
                     .depends()
-                        .artifact(new Artifact("org.testng/testng/5.10/jdk15"))
+                        .artifact(new Artifact("org.testng/testng-jdk15/5.10"))
                         .end()
                     .end()
                 .end()

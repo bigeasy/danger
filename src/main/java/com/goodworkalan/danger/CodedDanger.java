d76d1bd71540865b8dd387fd7a1ebff55a4f0315
Alan Gutierrez <alan@blogometer.com>
2010-06-17 18:33:59 -0500

    Wrote Javadoc.

--- src/main/java/com/goodworkalan/danger/CodedDanger.java
+++ src/main/java/com/goodworkalan/danger/CodedDanger.java
@@ -1,17 +1,26 @@
 package com.goodworkalan.danger;
 
-
+/**
+ * An exception class where exceptions are assigned an integer code to help
+ * distinguish them programatically, without the overhead of a class hierarchy.
+ * 
+ * @author Alan Gutierrez
+ */
 @SuppressWarnings("serial")
 public class CodedDanger extends Danger {
     /** The error code. */
     private final int code;
-    
-    /**
-     * Create a coded danger 
-     * @param bundles
-     * @param code
-     * @param cause
-     */
+
+	/**
+	 * Create a coded exception.
+	 * 
+	 * @param code
+	 *            The error code.
+	 * @param cause
+	 *            The cause.
+	 * @param arguments
+	 *            The format arguments.
+	 */
     public CodedDanger(int code, Throwable cause, Object...arguments) {
         super(null, Integer.toString(code), cause, arguments);
         this.code = code;
--- src/main/java/com/goodworkalan/danger/ContextualDanger.java
+++ src/main/java/com/goodworkalan/danger/ContextualDanger.java
@@ -1,32 +1,30 @@
 package com.goodworkalan.danger;
 
-
 /**
- * Good for extensible exceptions, because you can use the class name and
- * derived exception name as a namespace. Hmm...
+ * An excpetion that has string message codes, that uses a non-nested class to
+ * indicate the package where the message bundle is located.
  * <p>
- * Why not use the class name itself as a namespace? That is what I'm expecting.
- * <p>
- * Coded danger is extensible too. Just derive a type in your own package. You
- * can hide it. To avoid collisions in error codes, you can use a GitHub wiki
- * page to let people grab 100 value ranges.
- * <p>
- * Coded danger is especially useful for issues that you can recover from, but
- * not necessarily useful for errors that are programmer errors.
+ * The actual message key used to obtain a message from the message bundle is
+ * qualifed with the class name of the context class. The class name is
+ * catenated to the string code and separated by a slash, so that created an
+ * contextual exception with <code>com.acme.Account</code> and an string code of
+ * <code>overdrawn</code> would produce the message key
+ * <code>Account/overdrawn</code>. This allows a single bundle in a package to
+ * be used with multiple context classes from that package, without having to
+ * use unique string codes across the entire package.
  * 
  * @author Alan Gutierrez
  */
 @SuppressWarnings("serial")
 public class ContextualDanger extends Danger {
+	/** The message key. */
     private final String messageKey;
 
     /**
      * Create a contextual exception with the given context class, the given
      * string error code and the given positioned arguments.
      * 
-     * @param bundles
-     *            A cache of exception message bundles.
-     * @param context
+     * @param contextClass
      *            The error context.
      * @param code
      *            The error code.
@@ -38,10 +36,26 @@ public class ContextualDanger extends Danger {
         this.messageKey = getMessageKey(contextClass, code);
     }
     
+	/**
+	 * Get the message key, which is the context class name catenated with the
+	 * string code.
+	 * 
+	 * @return The message key.
+	 */
     public String getMessageKey() {
         return messageKey;
     }
 
+	/**
+	 * Create the qualified message key by contenting the context class name
+	 * with the message code.
+	 * 
+	 * @param contextClass
+	 *            The context class.
+	 * @param code
+	 *            The message code.
+	 * @return The qualified message key.
+	 */
     private static String getMessageKey(Class<?> contextClass, String code) {
         String className = contextClass.getCanonicalName();
         int dot = className.lastIndexOf('.');
